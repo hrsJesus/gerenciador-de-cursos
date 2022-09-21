89d41f97fd103b23d9218369fffb6c8bda197b65
Haroe Jesus <hrsilva.jesus@gmail.com>
2022-09-21 14:37:34 -0300

    login and session completed

---
+++ src/Controller/Persistencia.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
+class Persistencia implements InterfaceControladorRequisicao
+{
+    /**
+     * @var \Doctrine\ORM\EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct()
+    {
+        $this->entityManager = (new EntityManagerCreator())->getEntityManager();
+    }
+
+    public function processaRequisicao(): void
+    {
+        $descricao = strip_tags(filter_input(
+            INPUT_POST,
+            'descricao'
+        ));
+
+        $id = filter_input(
+            INPUT_GET,
+            'id',
+            FILTER_VALIDATE_INT
+        );
+
+        if (!is_null($id) && $id !== false) {
+            $curso = $this->entityManager->find(Curso::class, $id);
+            $curso->setDescricao($descricao);
+        } else {
+            $curso = new Curso();
+            $curso->setDescricao($descricao);
+            $this->entityManager->persist($curso);
+        }
+        $this->entityManager->flush();
+
+        header('Location: /listar-cursos', true, 302);
+    }
+}
